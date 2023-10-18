6e70bb1ac10c78e069b2cd6b7ca9b17e89b9639b
Ploy Supansa <92842420+PloySupansa@users.noreply.github.com>
2023-10-18 11:53:29 +0700

    Update footer.php

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
   <div class="copyright">
-    <p>Copyright © Developed by <a href="http://smethaisoftware.com/" target="_blank">SME THAI SOFTWARE</a> 2022</p>
+    <p>Copyright © Developed by <a href="https://smethaisoftware.com/" target="_blank">SME THAI SOFTWARE</a> 2022</p>
   </div>
 </div>
